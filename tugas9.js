a00705240ff775785afbef8f9b8c84fd7175f0d2
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-20 01:55:12 +0700

    Tugas 9 JS
    Tugas Javascript ini saya penuhi untuk menyelesaikan tugas 9 tentang for-in yang diberikan oleh NIOMIC

---
+++ tugas9.js
@@ -0,0 +1,11 @@
+var data = {
+  nama: "M. Ro'if",
+  umur: 20,
+  kotaLahir: "Sumenep",
+  angkatan: 2018,
+  fakultas: "Ilmu Komputer",
+  prodi: "Pendidikan Teknologi Informasi",
+};
+for (var tampil in data) {
+  console.log(data[tampil]);
+}
